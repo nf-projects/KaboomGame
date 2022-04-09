a6f35073598b86845a5db62653ade85e5940452d
nf-projects <nf-projects@users.noreply.github.com>
2022-04-08 21:21:13 -0700

    here we go

---
+++ README.md
@@ -0,0 +1,7 @@
+Created using [Kaboom.js](https://kaboomjs.com/)
+
+# Setup
+- `npm init`
+- `npm install kaboom`
+- `npm install -g esbuild`
+- `$ esbuild js/game.js --bundle > js/build.js`
---
+++ js/game.js
@@ -0,0 +1,21 @@
+import kaboom from "kaboom";
+
+// start the game
+kaboom()
+
+// load a default sprite
+loadBean()
+
+// add character to screen, from a list of components
+const player = add([
+    sprite("bean"),  // renders as a sprite
+    pos(120, 80),    // position in world
+    area(),          // has a collider
+    body(),          // responds to physics and gravity
+])
+
+// jump when player presses "space" key
+onKeyPress("space", () => {
+    // .jump() is provided by the body() component
+    player.jump()
+})
